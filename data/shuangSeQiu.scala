e8b3cbeecdaee30e7913b6936edd2b3b6ce786d7
BlankRain <731014656@qq.com>
2016-07-05 16:21:35 +0800

    双色球数据源
    根据APIURL抓取数据.返回String类型.未处理异常~

--- data/shuangSeQiu.scala
+++ data/shuangSeQiu.scala
@@ -1,1 +1,18 @@
-println("Hello World")
+import scalaj.http._
+object shuangSeQiu {
+
+   val apiUrl="http://www.lecai.com/lottery/draw/ajax_get_detail.php?lottery_type=50&phase=%s"
+
+   def getUrl(x:String):String ={
+     String.format(apiUrl,x)
+   }
+
+  /**
+   * 返回json字符串
+   * @param x 期号
+   * @return
+   */
+  def ssqData(x:String):String={
+    Http(getUrl(x)).asString.body
+  }
+}
